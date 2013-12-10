ab65be6986935088cfc41bbd1715915e6bc38def
Brian Neisler <neisler.brian@gmail.com>
2013-12-10 15:18:45 -0800

    Remove test code from prod build. Add encryption to both local and prod builds.

--- buildbug.js
+++ buildbug.js
@@ -38,12 +38,12 @@ buildProperties({
     buildbug: {
         packageJson: {
             name: "buildbug",
-            version: "0.0.19",
+            version: "0.0.20",
+            private: true,
             main: "./scripts/buildbug-module.js",
             bin: "bin/buildbug",
             dependencies: {
                 "aws-sdk": "1.9.x",
-                //bugjar: 'https://s3.amazonaws.com/bugjars/bugjar-0.0.1.tgz',
                 "bugpack-registry": 'https://s3.amazonaws.com/airbug/bugpack-registry-0.0.5.tgz',
                 bugpack: 'https://s3.amazonaws.com/airbug/bugpack-0.0.5.tgz',
                 bugunit: 'https://s3.amazonaws.com/airbug/bugunit-0.0.10.tgz',
@@ -57,29 +57,56 @@ buildProperties({
         },
         sourcePaths: [
             "../bugjs/projects/aws/js/src",
-            //"../bugjs/projects/buganno/js/src",
-            "../bugjs/projects/bugmeta/js/src",
             "../bugjs/projects/bugcli/js/src",
             "../bugjs/projects/bugflow/js/src",
             "../bugjs/projects/bugfs/js/src",
             "../bugjs/projects/bugjs/js/src",
+            "../bugjs/projects/bugmeta/js/src",
             "../bugjs/projects/bugtrace/js/src",
-            "../bugunit/projects/bugdouble/js/src",
-            "../bugunit/projects/bugunit/js/src",
             "./projects/buildbug/js/src"
         ],
         scriptPaths: [
             "../bugunit/projects/bugunit/js/scripts",
             "./projects/buildbug/js/scripts"
         ],
+        binPaths: [
+            "./projects/buildbug/bin"
+        ]
+    },
+    unitTest: {
+        packageJson: {
+            name: "buildbug-test",
+            version: "0.0.20",
+            private: true,
+            main: "./scripts/buildbug-module.js",
+            bin: "bin/buildbug",
+            dependencies: {
+                "aws-sdk": "1.9.x",
+                "bugpack-registry": 'https://s3.amazonaws.com/airbug/bugpack-registry-0.0.5.tgz',
+                bugpack: 'https://s3.amazonaws.com/airbug/bugpack-0.0.5.tgz',
+                bugunit: 'https://s3.amazonaws.com/airbug/bugunit-0.0.10.tgz',
+                deploybug: 'https://s3.amazonaws.com/airbug/deploybug-0.0.4.tgz',
+                lintbug: 'https://s3.amazonaws.com/airbug/lintbug-0.0.2.tgz',
+                "uglify-js": "2.3.x",
+                npm: '1.3.x',
+                tar: 'git://github.com/airbug/node-tar.git#master',
+                fstream: '0.1.x'
+            }
+        },
+        sourcePaths: [
+            "../bugunit/projects/bugdouble/js/src",
+            "../bugunit/projects/bugunit/js/src"
+        ],
+        scriptPaths: [
+            "../bugunit/projects/bugunit/js/scripts"
+        ],
         testPaths: [
             "../bugjs/projects/bugcli/js/test",
             "../bugjs/projects/bugflow/js/test",
             "../bugjs/projects/bugjs/js/test",
+            "../bugjs/projects/bugmeta/js/test",
+            "../bugjs/projects/bugtrace/js/test",
             "./projects/buildbug/js/test"
-        ],
-        binPaths: [
-            "./projects/buildbug/bin"
         ]
     }
 });
@@ -112,63 +139,76 @@ buildTarget('local').buildFlow(
         // old source files are removed. We should figure out a better way of doing that.
 
         targetTask('clean'),
-        targetTask('createNodePackage', {
-            properties: {
-                packageJson: buildProject.getProperty("buildbug.packageJson"),
-                sourcePaths: buildProject.getProperty("buildbug.sourcePaths"),
-                scriptPaths: buildProject.getProperty("buildbug.scriptPaths"),
-                testPaths: buildProject.getProperty("buildbug.testPaths"),
-                binPaths: buildProject.getProperty("buildbug.binPaths")
-            }
-        }),
-        targetTask('generateBugPackRegistry', {
-            init: function(task, buildProject, properties) {
-                var nodePackage = nodejs.findNodePackage(
-                    buildProject.getProperty("buildbug.packageJson.name"),
-                    buildProject.getProperty("buildbug.packageJson.version")
-                );
-                task.updateProperties({
-                    sourceRoot: nodePackage.getBuildPath()
-                });
-            }
-        }),
-        targetTask('packNodePackage', {
-            properties: {
-                packageName: buildProject.getProperty("buildbug.packageJson.name"),
-                packageVersion: buildProject.getProperty("buildbug.packageJson.version")
-            }
-        }),
-        targetTask('startNodeModuleTests', {
-            init: function(task, buildProject, properties) {
-                var packedNodePackage = nodejs.findPackedNodePackage(
-                    buildProject.getProperty("buildbug.packageJson.name"),
-                    buildProject.getProperty("buildbug.packageJson.version")
-                );
-                task.updateProperties({
-                    modulePath: packedNodePackage.getFilePath()
-                });
-            }
-        }),
-        targetTask("s3EnsureBucket", {
-            properties: {
-                bucket: buildProject.getProperty("local-bucket")
-            }
-        }),
-        targetTask("s3PutFile", {
-            init: function(task, buildProject, properties) {
-                var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("buildbug.packageJson.name"),
-                    buildProject.getProperty("buildbug.packageJson.version"));
-                task.updateProperties({
-                    file: packedNodePackage.getFilePath(),
-                    options: {
-                        acl: 'public-read'
+        series([
+            parallel([
+                targetTask("s3EnsureBucket", {
+                    properties: {
+                        bucket: buildProject.getProperty("local-bucket")
                     }
-                });
-            },
-            properties: {
-                bucket: buildProject.getProperty("local-bucket")
-            }
-        })
+                }),
+                series([
+                    targetTask('createNodePackage', {
+                        properties: {
+                            packageJson: buildProject.getProperty("buildbug.packageJson"),
+                            sourcePaths: buildProject.getProperty("buildbug.sourcePaths").concat(
+                                buildProject.getProperty("unitTest.sourcePaths")
+                            ),
+                            scriptPaths: buildProject.getProperty("buildbug.scriptPaths").concat(
+                                buildProject.getProperty("unitTest.scriptPaths")
+                            ),
+                            testPaths: buildProject.getProperty("unitTest.testPaths"),
+                            binPaths: buildProject.getProperty("buildbug.binPaths")
+                        }
+                    }),
+                    targetTask('generateBugPackRegistry', {
+                        init: function(task, buildProject, properties) {
+                            var nodePackage = nodejs.findNodePackage(
+                                buildProject.getProperty("buildbug.packageJson.name"),
+                                buildProject.getProperty("buildbug.packageJson.version")
+                            );
+                            task.updateProperties({
+                                sourceRoot: nodePackage.getBuildPath()
+                            });
+                        }
+                    }),
+                    targetTask('packNodePackage', {
+                        properties: {
+                            packageName: buildProject.getProperty("buildbug.packageJson.name"),
+                            packageVersion: buildProject.getProperty("buildbug.packageJson.version")
+                        }
+                    }),
+                    targetTask('startNodeModuleTests', {
+                        init: function(task, buildProject, properties) {
+                            var packedNodePackage = nodejs.findPackedNodePackage(
+                                buildProject.getProperty("buildbug.packageJson.name"),
+                                buildProject.getProperty("buildbug.packageJson.version")
+                            );
+                            task.updateProperties({
+                                modulePath: packedNodePackage.getFilePath()
+                            });
+                        }
+                    })
+                ])
+            ]),
+            targetTask("s3PutFile", {
+                init: function(task, buildProject) {
+                    var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("buildbug.packageJson.name"),
+                        buildProject.getProperty("buildbug.packageJson.version"));
+                    task.updateProperties({
+                        file: packedNodePackage.getFilePath(),
+                        options: {
+
+                            //TODO BRN: In order to protect this file we need to limit the access to this artifact and provide some sort of http auth access so that the artifacts are retrievable via npm install. This would need to be done in a server wrapper.
+
+                            acl: 'public-read'
+                        }
+                    });
+                },
+                properties: {
+                    bucket: buildProject.getProperty("local-bucket")
+                }
+            })
+        ])
     ])
 ).makeDefault();
 
@@ -183,62 +223,108 @@ buildTarget("prod").buildFlow(
         // old source files are removed. We should figure out a better way of doing that.
 
         targetTask("clean"),
-        targetTask("createNodePackage", {
-            properties: {
-                packageJson: buildProject.getProperty("buildbug.packageJson"),
-                sourcePaths: buildProject.getProperty("buildbug.sourcePaths"),
-                scriptPaths: buildProject.getProperty("buildbug.scriptPaths"),
-                testPaths: buildProject.getProperty("buildbug.testPaths"),
-                binPaths: buildProject.getProperty("buildbug.binPaths")
-            }
-        }),
-        targetTask("generateBugPackRegistry", {
-            init: function(task, buildProject, properties) {
-                var nodePackage = nodejs.findNodePackage(
-                    buildProject.getProperty("buildbug.packageJson.name"),
-                    buildProject.getProperty("buildbug.packageJson.version")
-                );
-                task.updateProperties({
-                    sourceRoot: nodePackage.getBuildPath()
-                });
-            }
-        }),
-        targetTask("packNodePackage", {
-            properties: {
-                packageName: buildProject.getProperty("buildbug.packageJson.name"),
-                packageVersion: buildProject.getProperty("buildbug.packageJson.version")
-            }
-        }),
-        targetTask("startNodeModuleTests", {
-            init: function(task, buildProject, properties) {
-                var packedNodePackage = nodejs.findPackedNodePackage(
-                    buildProject.getProperty("buildbug.packageJson.name"),
-                    buildProject.getProperty("buildbug.packageJson.version")
-                );
-                task.updateProperties({
-                    modulePath: packedNodePackage.getFilePath()
-                });
-            }
-        }),
-        targetTask("s3EnsureBucket", {
-            properties: {
-                bucket: "airbug"
-            }
-        }),
-        targetTask("s3PutFile", {
-            init: function(task, buildProject, properties) {
-                var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("buildbug.packageJson.name"),
-                    buildProject.getProperty("buildbug.packageJson.version"));
-                task.updateProperties({
-                    file: packedNodePackage.getFilePath(),
-                    options: {
-                        acl: "public-read"
+        parallel([
+            series([
+                targetTask("createNodePackage", {
+                    properties: {
+                        packageJson: buildProject.getProperty("buildbug.packageJson"),
+                        sourcePaths: buildProject.getProperty("buildbug.sourcePaths").concat(
+                            buildProject.getProperty("unitTest.sourcePaths")
+                        ),
+                        scriptPaths: buildProject.getProperty("buildbug.scriptPaths").concat(
+                            buildProject.getProperty("unitTest.scriptPaths")
+                        ),
+                        testPaths: buildProject.getProperty("unitTest.testPaths"),
+                        binPaths: buildProject.getProperty("buildbug.binPaths")
                     }
-                });
-            },
-            properties: {
-                bucket: "airbug"
-            }
-        })
+                }),
+                targetTask("generateBugPackRegistry", {
+                    init: function(task, buildProject, properties) {
+                        var nodePackage = nodejs.findNodePackage(
+                            buildProject.getProperty("buildbug.packageJson.name"),
+                            buildProject.getProperty("buildbug.packageJson.version")
+                        );
+                        task.updateProperties({
+                            sourceRoot: nodePackage.getBuildPath()
+                        });
+                    }
+                }),
+                targetTask("packNodePackage", {
+                    properties: {
+                        packageName: buildProject.getProperty("buildbug.packageJson.name"),
+                        packageVersion: buildProject.getProperty("buildbug.packageJson.version")
+                    }
+                }),
+                targetTask("startNodeModuleTests", {
+                    init: function(task, buildProject, properties) {
+                        var packedNodePackage = nodejs.findPackedNodePackage(
+                            buildProject.getProperty("buildbug.packageJson.name"),
+                            buildProject.getProperty("buildbug.packageJson.version")
+                        );
+                        task.updateProperties({
+                            modulePath: packedNodePackage.getFilePath()
+                        });
+                    }
+                })
+            ]),
+
+            // Create production package
+
+            series([
+                parallel([
+                    targetTask("s3EnsureBucket", {
+                        properties: {
+                            bucket: "airbug"
+                        }
+                    }),
+                    series([
+                        targetTask("createNodePackage", {
+                            properties: {
+                                packageJson: buildProject.getProperty("buildbug.packageJson"),
+                                sourcePaths: buildProject.getProperty("buildbug.sourcePaths"),
+                                scriptPaths: buildProject.getProperty("buildbug.scriptPaths"),
+                                binPaths: buildProject.getProperty("buildbug.binPaths")
+                            }
+                        }),
+                        targetTask("generateBugPackRegistry", {
+                            init: function(task, buildProject) {
+                                var nodePackage = nodejs.findNodePackage(
+                                    buildProject.getProperty("buildbug.packageJson.name"),
+                                    buildProject.getProperty("buildbug.packageJson.version")
+                                );
+                                task.updateProperties({
+                                    sourceRoot: nodePackage.getBuildPath()
+                                });
+                            }
+                        }),
+                        targetTask("packNodePackage", {
+                            properties: {
+                                packageName: buildProject.getProperty("buildbug.packageJson.name"),
+                                packageVersion: buildProject.getProperty("buildbug.packageJson.version")
+                            }
+                        })
+                    ])
+                ]),
+                targetTask("s3PutFile", {
+                    init: function(task, buildProject, properties) {
+                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("buildbug.packageJson.name"),
+                            buildProject.getProperty("buildbug.packageJson.version"));
+                        task.updateProperties({
+                            file: packedNodePackage.getFilePath(),
+                            options: {
+
+                                //TODO BRN: In order to protect this file we need to limit the access to this artifact and provide some sort of http auth access so that the artifacts are retrievable via npm install. This would need to be done in a server wrapper.
+
+                                acl: "public-read",
+                                encrypt: true
+                            }
+                        });
+                    },
+                    properties: {
+                        bucket: "airbug"
+                    }
+                })
+            ])
+        ])
     ])
 );
